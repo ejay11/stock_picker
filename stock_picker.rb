282cf59e65d66174ee151245cb76e85dddceaa2e
ejay11 <ellen.jaquette@gmail.com>
2021-10-15 21:10:47 -0500

    consistent naming, removed unnecessary profit_list

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,64 +1,24 @@
-#Returns Nested Hash - key is array[key] value or the buy price, array value for key is profit that buy price would get for the remaining sell prices. 
-def stock_picker (array)
-  profit_list = []
-  best_days = []
-  x = 0
-  y = 1
-
-  # while x < array.length
-
-  # Puts profits of every sale into an array
-    # while y < array.length
-      # profit_list.push(array[y] - array[x])
-      # y += 1
-    # end
-
-    # if y > array.length && x > array.length
-      # profit_list
-    # else     
-      # y = x + 2
-    # end
-    # x += 1
-  # end
-  
-  # Instead of while loops and manually incrementing the index, each_with_index can do that for you
-  # For each day, we take the days not in the past and get the profit.
-  bestProfitInfo = [] # a triple containing profit, buy, sell days. maybe a hash would be nicer.
-  array.each_with_index do |bp, bi|
-	noPast = array[bi..-1]
-	noPast.each_with_index do |sp, si|
-		profit = sp - bp
-		profit_list.push(profit) # Don't really need this if we do the below.
-		# At this point I can go with the loop below and be good, but it's effectively doing the same work
-		# twice of calculating all the profits, so instead I can capture the index values here.
-		sellDay = bi + si
-		todayProfitInfo = [profit, bi, sellDay]
-		puts "Buying on day #{bi} at #{bp} and selling on day #{sellDay} for #{sp} is a profit of #{profit}."
-		if (bestProfitInfo == [])
-			bestProfitInfo = todayProfitInfo
-		elsif (todayProfitInfo[0] > bestProfitInfo[0])
-			puts "It's the best profit so far!"
-			bestProfitInfo = todayProfitInfo
+def stock_picker (array)  
+  best_profit = []
+  array.each_with_index do |buy_price, buy_index|
+    #create new array beginning right after buy index and going to whatever the end is
+	  noPast = array[buy_index..-1]
+	    noPast.each_with_index do |sell_price, sell_index|
+		    profit = sell_price - buy_price
+		    sell_day = buy_index + sell_index
+		    today_profit = [profit, buy_index, sell_day]
+		    #puts "Buying on day #{buy_index} at #{buy_price} and selling on day #{sell_day} for #{sell_price} is a profit of #{profit}."
+		    if (best_profit == [])
+			    best_profit = today_profit
+		    elsif (today_profit[0] > best_profit[0])
+			  #puts "It's the best profit so far!"
+			  best_profit = today_profit
 		end
 	end
 end
-	puts "Best profit is $#{bestProfitInfo[0]} on days #{bestProfitInfo[1..2]}."
-	return bestProfitInfo[1..2]
+	puts "Best profit is $#{best_profit[0]} on days #{best_profit[1..2]}."
+	return best_profit[1..2]
   
-
-# largest_profit = profit_list.max
-
-#Finds and returns the array indexes for the best buy and sell prices to get largest_profit
-# array.each_with_index do |buy, index|
-  # y = 0
-  # while y < array.length
-    # if array[y] - buy == largest_profit
-      # best_days.push(index, y)
-      # print best_days
-    # end
-    # y += 1
-  # end
-# end
 end
 
 
